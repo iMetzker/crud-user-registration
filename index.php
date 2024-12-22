7c6a65a1a91dafeb995869d9dcc0106376919e1c
Metzker <ivnymetzker@gmail.com>
2024-12-21 21:38:42 -0300

    :lipstick: style: adding bootstrap files

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Usuários do Sistema</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Usuários do Sistema</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Início</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="novo-usuario.php">
+            Novo Usuário</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="listar-usuario.php">Listar Usuários</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
